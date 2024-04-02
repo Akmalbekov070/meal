ce4fbc6b7619c8b7d39fdb22e315967a7babf876
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-04-02 22:12:49 +0300

    media abaou page

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -4,8 +4,8 @@ import { FaChevronRight } from 'react-icons/fa';
 
 export default function Blog() {
 	return (
-		<Box w={'full'} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} pt={'20'}>
-			<Box w={'50%'} border={'1px'} borderColor={'white'} p={10} boxShadow={'dark-lg'} borderRadius={'10'}>
+		<Box w={'full'} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} py={'20'}>
+			<Box w={{ base: '100%', xl: '50%' }} border={'1px'} borderColor={'white'} p={10} boxShadow={'dark-lg'} borderRadius={'10'}>
 				<Text>
 					On this site you can get your favorite food recipes and cook it and the plus side of this site is that you can cook it
 					via video.If you want to get more information about the site, contact us
@@ -14,9 +14,9 @@ export default function Blog() {
 					MERN stack & Web Developer.
 				</Text>
 				<Box w={'full'} bg={'red.800'} h={'2px'} my={2}></Box>
-				<Box w={'full'} display={'flex'} px={10} pt={2}>
-					<Box w={'full'} display={'flex'} flexDirection={'column'}>
-						<HStack>
+				<Box w={'full'} display={{ base: 'block', xl: 'flex' }} px={10} pt={2}>
+					<Box w={'full'} display={{ base: 'block', xl: 'flex' }} flexDirection={'column'}>
+						<HStack w={'full'}>
 							<FaChevronRight color='red' />
 							<Text> Country: Uzbekistan </Text>
 						</HStack>
