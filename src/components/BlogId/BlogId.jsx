5e5e53489ad75c25e68414658f6611b5a23bb427
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-06-14 23:53:23 +0300

    code

--- src/components/BlogId/BlogId.jsx
+++ src/components/BlogId/BlogId.jsx
@@ -11,7 +11,46 @@ import { formSchema } from '@/lib/[validation]';
 
 export default function BlogId() {
 	const { colorMode, toggleColorMode } = useColorMode();
+	const [load, setLoad] = useState(false);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			username: '',
+			phone: '',
+			message: '',
+		},
+	});
 
+	const onSubmit = values => {
+		setLoad(true);
+		const telegramBotId = process.env.NEXT_PUBLIC_TELEGRAM_API;
+		const telegramBotKey = process.env.NEXT_PUBLIC_TELEGRAM_KEY_API;
+
+		const promise = fetch(`https://api.telegram.org/bot${telegramBotId}/sendMessage`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Cache-Control': 'no-cache',
+			},
+			body: JSON.stringify({
+				chat_id: telegramBotKey,
+				text: `Name: ${values.username},\nPhone: ${values.phone},\nMessage: ${values.message}`,
+			}),
+		})
+			.then(() => reset())
+			.finally(() => setLoad(false));
+
+		toast.promise(promise, {
+			loading: 'Loading...',
+			success: 'Successfully sent',
+			error: 'Error occurred',
+		});
+	};
 	function onSubmit(values) {
 		console.log(values);
 	}
